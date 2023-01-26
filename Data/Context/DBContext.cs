cca320a99aca758cf74738d0ff50248477235f11
Reinaldo Sales <httpsreinaldo@gmail.com>
2023-01-26 16:06:50 -0300

    udpate a file

--- Data/Context/DBContext.cs
+++ Data/Context/DBContext.cs
@@ -6,7 +6,7 @@ namespace Data.Context
 {
     internal class DBContext
     {
-        private readonly string _connectionStrings = "Data Source=172.26.0.70\\COLLECTION;Initial Catalog=fb_servicesystem;User ID=BDCOLLECTION;Password=HFij49O5JO3Q;Pooling=True;Connect Timeout=180; Application Name=Integration System;Trusted_Connection=False; TrustServerCertificate=True";
+        private readonly string _connectionStrings = "Data Source=bd;Initial Catalog=db;User ID=db;Password=db;Pooling=True;Connect Timeout=180; Application Name=Integration System;Trusted_Connection=False; TrustServerCertificate=True";
 
         private readonly string _sqliteConnectionStrings = "Data Source=C:\\DapperCrud.db";
 
